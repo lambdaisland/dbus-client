8d442c313e27e1458236e64df1432c7dc07a18fe
Arne Brasseur <arne@arnebrasseur.net>
2025-10-01 17:33:22 +0200

    wip

---
+++ src/lambdaisland/dbus/client.clj
@@ -0,0 +1,88 @@
+(ns lambdaisland.dbus.client
+  (:require
+   [lambdaisland.dbus.format :as format])
+  (:import
+   (java.net UnixDomainSocketAddress)
+   (java.nio ByteBuffer)
+   (java.nio.channels SocketChannel)
+   (java.nio.charset StandardCharsets)))
+
+(def null-byte "\u0000")
+(def crlf "\r\n")
+
+(def auth-external (str null-byte "AUTH EXTERNAL" crlf))
+(def data-cmd (str "DATA" crlf))
+(def negotiate-unix-fd (str "NEGOTIATE_UNIX_FD" crlf))
+(def begin-cmd (str "BEGIN" crlf))
+
+(def ok-response (str "OK" crlf))
+(def agree-unix-fd (str "AGREE_UNIX_FD" crlf))
+
+(defn write-str [^SocketChannel chan ^String s]
+  (let [^bytes b (.getBytes s)]
+    (loop [offset 0]
+      (when (< offset (count b))
+        (let [buf (ByteBuffer/wrap b offset (- (count b) offset))
+              written (.write chan buf)]
+          (recur (+ offset written)))))))
+
+(defn read-handshake-lines [^SocketChannel chan ^ByteBuffer buffer]
+  (let [cr 13, lf 10, crlf-len 2]
+    (when (pos? (.remaining buffer))
+      (.read chan buffer))
+    (.flip buffer)
+
+    (loop [lines []]
+      (let [limit (.limit buffer)
+            pos (.position buffer)
+            remaining (- limit pos)]
+        (if (< remaining crlf-len)
+          (do (.compact buffer) lines)
+          (let [match-pos (loop [i pos]
+                            (if (>= (+ i crlf-len) limit)
+                              -1 ; Delimiter not found in remaining data
+                              (if (and (= cr (.get buffer i))
+                                       (= lf (.get buffer (inc i))))
+                                i ; Match found at index i
+                                (recur (inc i)))))]
+
+            (if (= match-pos -1)
+              ;; Delimiter not found: Terminate scan.
+              (do (.compact buffer) lines)
+
+              ;; Delimiter Found: Extract line and recurse
+              (let [line-length (+ (- match-pos pos) crlf-len)
+                    line-bytes (byte-array line-length)
+                    _ (.get buffer line-bytes 0 line-length) ; Reads from current position (pos) & moves position
+                    line-str (String. line-bytes StandardCharsets/UTF_8)]
+
+                ;; Position is already updated by the .get call!
+                (recur (conj lines line-str))))))))))
+
+(def hello-call
+  {:type :method-call
+   :flags {}
+   :version 1
+   :serial 1
+   :headers
+   {:path "/org/freedesktop/DBus"
+    :member "Hello"
+    :interface "org.freedesktop.DBus"
+    :destination "org.freedesktop.DBus"}})
+
+(defn handshake [chan]
+  (write-str chan
+             (str auth-external
+                  data-cmd
+                  negotiate-unix-fd
+                  begin-cmd))
+  (let [buf (format/byte-buffer)
+        id (loop [[line & lines] (read-handshake-lines chan buf)]
+             (if-let [[_ id] (re-find #"OK ([0-9a-f]*)\r\n" line)]
+               id
+               (recur lines)))]
+    (format/sock-write chan format/write-message hello-call)
+    {:socket chan
+     :buffer buf
+     :id id
+     :assigned-name (ffirst (:body (msg/read-message (ByteBuffer/wrap (msg/sock-read chan)))))}))
---
+++ src/lambdaisland/dbus/format.clj
@@ -0,0 +1,355 @@
+(ns lambdaisland.dbus.format
+  "Read/write the DBUS message format"
+  (:require
+   [clojure.java.io :as io])
+  (:import
+   (java.net ServerSocket StandardProtocolFamily UnixDomainSocketAddress)
+   (java.nio ByteBuffer ByteOrder)
+   (java.nio.channels ServerSocketChannel SocketChannel)))
+
+(set! *warn-on-reflection* true)
+
+(def ^:dynamic *default-buffer-size* 4096)
+
+(defn byte-buffer ^ByteBuffer []
+  (ByteBuffer/allocate *default-buffer-size*))
+
+(declare get-array get-byte get-signature get-string get-string get-uint32
+         put-array put-byte put-signature put-string put-string put-uint32
+         sig->type)
+
+(def types
+  [{:id :bool :sig \b}
+   {:id :byte :sig \y :read get-byte :write put-byte}
+   {:id :double :sig \d}
+   {:id :int16 :sig \n}
+   {:id :int32 :sig \i}
+   {:id :int64 :sig \x}
+   {:id :object-path :sig \o :read get-string :write put-string}
+   {:id :signature :sig \g :read get-signature :write put-signature}
+   {:id :string :sig \s :read get-string :write put-string}
+   {:id :uint16 :sig \q}
+   {:id :uint32 :sig \u :read get-uint32 :write put-uint32}
+   {:id :uint64 :sig \t}
+   {:id :array :sig \a :read get-array :write put-array}
+   {:id :variant :sig \v}
+   {:id :struct :sig \( :sig-end\)}])
+
+(def message-types
+  [:invalid :method-call :method-return :error :signal])
+
+(def headers
+  [[:invalid nil]
+   [:path :object-path]
+   [:interface :string]
+   [:member :string]
+   [:error-name :string]
+   [:reply-serial :uint32]
+   [:destination :string]
+   [:sender :string]
+   [:signature :signature]
+   [:unix-fds :uint32]])
+
+(def sig->type* (into {} (map (juxt :sig :id)) types))
+(def type->sig* (into {} (map (juxt :id :sig)) types))
+(def type->write-fn* (into {} (map (juxt :id :write)) types))
+(def type->read-fn* (into {} (map (juxt :id :read)) types))
+
+(def offset 0)
+
+(defn align [^ByteBuffer buf size]
+  (dotimes [_ (mod (- size (mod (- (.position buf) offset) size)) size)]
+    (.put buf (byte 0)))
+  buf)
+
+(defn get-byte [^ByteBuffer buf]
+  (bit-and (.get buf) 0xff))
+
+(defn get-uint32 [^ByteBuffer buf]
+  (align buf 4)
+  (bit-and (.getInt buf) 0xffffffff))
+
+(defn get-array [^ByteBuffer buf read-fn]
+  (align buf 4)
+  (let [len (get-uint32 buf)
+        end (+ (.position buf) len)]
+    (loop [res []]
+      (if (< (.position buf) end)
+        (recur (conj res (doto (read-fn buf) prn)))
+        res))))
+
+(defn get-struct [^ByteBuffer buf read-fns]
+  (align buf 8)
+  (mapv #(% buf) read-fns))
+
+(defn get-string [^ByteBuffer buf]
+  (align buf 4)
+  (let [len (get-uint32 buf)
+        res (String. ^bytes (let [ba (byte-array len)]
+                              (.get buf ba 0 len)
+                              ba))]
+    (.get buf) ;; NULL
+    res))
+
+(defn get-signature [^ByteBuffer buf]
+  (let [len (get-byte buf)
+        res (String. ^bytes (let [ba (byte-array len)]
+                              (.get buf ba 0 len)
+                              ba))]
+    (.get buf) ;; NULL
+    res))
+
+(defn read-type [buf t]
+  ((or (type->read-fn* t)
+       (cond
+         (vector? t)
+         (case (first t)
+           :tuple
+           (if (= 1 (count t))
+             (constantly nil)
+             (fn [buf]
+               (for [t (rest t)]
+                 (read-type buf t))))
+           :variant
+           (fn [buf]
+             (let [sig (get-signature buf)]
+               (read-type buf (sig->type sig)))))
+
+         :else
+         (throw (ex-info  "unimplemented type" {:t t}))))
+   buf))
+
+(defn type->sig [t]
+  (if (vector? t)
+    (case (first t)
+      :tuple
+      (apply str (map type->sig (rest t)))
+      :array
+      (str "a" (type->sig (second t)))
+      :struct
+      (str "(" (apply str (map type->sig (rest t))) ")")
+      :variant
+      "v")
+    (str (type->sig* t))))
+
+(defn sig->type [sig]
+  (if (char? sig)
+    (sig->type* sig)
+    (loop [t [:tuple]
+           [c & cs] sig]
+      (if (not c)
+        t
+        (case c
+          \(
+          (recur
+           (conj t
+                 (into [:struct] (next (sig->type
+                                        (take-while (complement #{\)}) cs)))))
+           (next (drop-while (complement #{\)}) cs)))
+          \a
+          (recur
+           (conj t [:array (sig->type (first cs))])
+           (next cs))
+
+          \v
+          (let [[_tuple & ts] (sig->type cs)]
+            (into (conj t [:variant (first ts)]) (next ts)))
+
+          #_else
+          (recur (conj t (sig->type c))
+                 cs))))))
+
+(defn read-header [^ByteBuffer buf]
+  (align buf 8)
+  (let [code (get-byte buf)
+        ;; _ (prn "<H" (.position buf) code)
+        _ (assert (< 0 code 10) code)
+        [h t] (nth headers code)
+        sig (get-signature buf)
+        v (read-type buf (sig->type sig))]
+    ;; (prn [(.position buf) h t v])
+    [h v]))
+
+(defn read-message [^ByteBuffer buf]
+  (let [endian (.get buf)
+        _ (.order buf
+                  (case (char endian)
+                    \l
+                    ByteOrder/LITTLE_ENDIAN
+                    \B
+                    ByteOrder/BIG_ENDIAN))
+        msg-type (nth message-types (get-byte buf))
+        flags (let [flags (get-byte buf)]
+                (cond-> {}
+                  (= 0x1 (bit-and flags 0x1))
+                  (assoc :no-reply-expected true)
+                  (= 0x2 (bit-and flags 0x2))
+                  (assoc :no-auto-start true)
+                  (= 0x4 (bit-and flags 0x4))
+                  (assoc :allow-interactive-authorization true)))
+        version (get-byte buf)
+        len  (get-uint32 buf)
+        serial  (get-uint32 buf)
+        headers (into {} (get-array buf read-header))]
+    {:endian (char endian)
+     :type msg-type
+     :flags flags
+     :version version
+     :body-length len
+     :serial serial
+     :headers headers
+     :body (read-type buf (sig->type (get headers :signature)))}))
+
+(defn put-string [^ByteBuffer buf s]
+  (align buf 4)
+  (let [b (.getBytes ^String s)]
+    (.putInt buf (count b))
+    (.put buf b)
+    (.put buf (byte 0))))
+
+(defn put-signature [^ByteBuffer buf s]
+  (let [b (.getBytes ^String s)]
+    (.put buf (byte (count b)))
+    (.put buf b)
+    (.put buf (byte 0))))
+
+(defn put-byte [^ByteBuffer buf v]
+  (.put buf (byte (bit-and (long v) 0xff))))
+
+(defn put-uint32 [^ByteBuffer buf v]
+  (.putInt buf (int (bit-and (long v) 0xffffffff))))
+
+(defn write-array [^ByteBuffer buf write-elements-fn]
+  (align buf 4)
+  (let [size-pos (.position buf)]
+    (.putInt buf 0)
+    (write-elements-fn buf)
+    (let [end-pos (.position buf)]
+      (.position buf size-pos)
+      (.putInt buf (- end-pos size-pos 4))
+      (.position buf end-pos)))
+  buf)
+
+(defn show-buffer-lim [^ByteBuffer b]
+  (let [p (.limit b)]
+    (.position b 0)
+    (repeatedly p #(.get b))))
+
+(defn show-buffer-pos [^ByteBuffer b]
+  (let [p (.position b)]
+    (.position b 0)
+    (repeatedly p #(.get b))))
+
+(defn write-type [buf t v]
+  ((or
+    (type->write-fn* t)
+    (cond
+      (vector? t)
+      (case (first t)
+        :tuple
+        (if (= 1 (count t))
+          (constantly nil)
+          (fn [buf vs]
+            (doall
+             (map (partial write-type buf) (rest t) vs))))
+        :variant
+        (fn [buf v]
+          (put-signature buf (type->sig (second t)))
+          (write-type buf (second t) v)))
+
+      :else
+      (throw (ex-info  "unimplemented type" {:t t}) )))
+   buf v))
+
+(defn write-headers [^ByteBuffer buf header-map]
+  (let [hidx (into {} (map-indexed (fn [idx [k v]] [k idx]) headers))
+        headers (into {} headers)]
+    (doseq [[k v] header-map]
+      (align buf 8)
+      (let [t (get headers k)
+            code (get hidx k)]
+        (println ["H" k code t v (.position buf)])
+        (put-byte buf code)
+        (write-type buf [:variant t] v)))
+    buf))
+
+(defn write-message [^ByteBuffer buf {:keys [type flags headers version serial body]
+                                      :or {version 1}}]
+  (let [endian (.order buf)]
+    (put-byte buf (if (= endian ByteOrder/LITTLE_ENDIAN)
+                    \l
+                    \B))
+    (put-byte buf (.indexOf ^java.util.List message-types type))
+    (put-byte buf (cond-> 0
+                    (:no-reply-expected flags)
+                    (bit-or 1)
+                    (:no-auto-start flags)
+                    (bit-or 2)
+                    (:allow-interactive-authorization flags)
+                    (bit-or 3)))
+    (put-byte buf version)
+
+    (let [body-length-pos (.position buf)]
+      (put-uint32 buf 0) ;;body length placeholder
+      (put-uint32 buf serial)
+
+      (write-array buf #(write-headers % headers))
+      (align buf 8)
+
+      (let [body-start-pos (.position buf)]
+        (write-type buf (sig->type (get headers :signature)) body)
+        (let [body-end-pos (.position buf)
+              body-len (- body-end-pos body-start-pos)]
+          (.putInt buf body-length-pos body-len))))
+    buf))
+
+(defn sock-read ^bytes [^SocketChannel chan]
+  (let [buf (byte-buffer)]
+    (.mark buf)
+    (let [len (.read chan buf)]
+      (if (< 0 len)
+        (let [arr (byte-array len)]
+          (.flip buf)
+          (.get buf arr 0 len)
+          arr)
+        (do
+          (println "WARN: read from closed channel")
+          (byte-array 0))))))
+
+(defn sock-write
+  ([^SocketChannel chan ^String s]
+   (let [buf (byte-buffer)]
+     (.mark buf)
+     (.put buf (.getBytes s))
+     (.flip buf)
+     (.write chan buf)))
+  ([^SocketChannel chan f & args]
+   (let [buf (byte-buffer)]
+     (.mark buf)
+     (apply f buf args)
+     (.flip buf)
+     (.write chan buf))))
+
+(defn write-to-str
+  "Testing utility, takes a function that takes a buffer as its first argument,
+  and any arguments to that function, calls it on a new buffer, then converts to
+  String."
+  [f & args]
+  (let [^ByteBuffer b (byte-buffer)]
+    (.mark b)
+    (apply f b args)
+    (let [len (.position b)
+          arr (byte-array len)]
+      (.flip b)
+      (.get b arr 0 len)
+      (String. arr))))
+
+(defn sock-conn ^SocketChannel [^String sock-loc]
+  (SocketChannel/open (UnixDomainSocketAddress/of sock-loc)))
+
+(defn session-sock []
+  (let [[_ path] (re-find #"unix:path=(.*)" (System/getenv "DBUS_SESSION_BUS_ADDRESS"))]
+    (sock-conn path)))
+
+(defn system-sock []
+  (sock-conn "/run/dbus/system_bus_socket"))
